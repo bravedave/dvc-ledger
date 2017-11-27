94c5df327f802159157e5afbfd3245994475c2a7
bravedave <davbray@gmail.com>
2017-11-27 12:45:31 +1000

    moment is supplied by the core

--- application/app/page.php
+++ application/app/page.php
@@ -9,13 +9,20 @@
 
 	*/
 class page extends dvc\pages\bootstrap {
+	static $addScripts = TRUE;
+
 	function __construct( $title = '' ) {
 		self::$BootStrap_Version = '4';
+		self::$momentJS = self::$addScripts;
+
 		parent::__construct( $title );
+
+		if ( !self::$addScripts)
+			return;
 
 		$this->css[] = sprintf( '<link type="text/css" rel="stylesheet" media="all" href="%s" />', url::tostring('css/jquery-ui.min.css'));
 		$this->scripts[] = sprintf( '<script type="text/javascript" src="%s"></script>', url::tostring('js/jquery-ui.min.js'));
-		$this->scripts[] = sprintf( '<script type="text/javascript" src="%s"></script>', url::tostring('js/moment.min.js'));
+		// $this->scripts[] = sprintf( '<script type="text/javascript" src="%s"></script>', url::tostring('js/moment.min.js'));
 		$this->scripts[] = sprintf( '<script type="text/javascript" src="%s"></script>', url::tostring('script'));
 
 	}
